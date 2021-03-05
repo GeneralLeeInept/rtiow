1e64df32f529429a13b4949e7783fcf0b9a46f7e
Derek Burnheim <derek@hemicube.com>
2021-03-05 15:12:40 +1100

    2. Output an image (with progress indicator)
    Using stb to write image out as a PNG.

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,56 @@
-// rtiow.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "stb_image_write.h"
 
+#include <cstdint>
 #include <iostream>
+#include <memory>
+#include <string_view>
+
+struct Image
+{
+    uint32_t width;
+    uint32_t height;
+    std::unique_ptr<uint8_t[]> bits;
+};
+
+std::unique_ptr<Image> createImage(uint32_t width, uint32_t height)
+{
+    std::unique_ptr<Image> image = std::make_unique<Image>();
+    image->width = width;
+    image->height = height;
+    image->bits = std::make_unique<uint8_t[]>(width * height * 3);
+    return image;
+}
+
+bool saveImage(Image& image, const std::string_view& path)
+{
+    stbi_flip_vertically_on_write(1);
+    return !!stbi_write_png(path.data(), image.width, image.height, 3, image.bits.get(), image.width * 3);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto image = createImage(640, 480);
+
+    for (int j = 640-1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+        for (int i = 0; i < 480; ++i)
+        {
+            auto r = double(i) / (480-1);
+            auto g = double(j) / (640-1);
+            auto b = 0.25;
+
+            uint8_t ir = static_cast<uint8_t>(255.999 * r);
+            uint8_t ig = static_cast<uint8_t>(255.999 * g);
+            uint8_t ib = static_cast<uint8_t>(255.999 * b);
+
+            image->bits[(i * 640 * 3 + j * 3) + 0] = ir;
+            image->bits[(i * 640 * 3 + j * 3) + 1] = ig;
+            image->bits[(i * 640 * 3 + j * 3) + 2] = ib;
+        }
+    }
+
+    saveImage(*image, "image.png");
+    std::cerr << "\nDone.\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
